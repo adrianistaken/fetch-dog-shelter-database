5653743e69aa67c9d2ae6409e1d41862904e709c
adrianistaken <adrian.rodriguez.dev@gmail.com>
2025-02-21 18:37:37 -0600

    Created simple Login view

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,11 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+import Login from "./components/Login.vue";
 </script>
 
 <template>
-  <div>
-    <h1>Fetch</h1>
-    <h2>Find Your Future Best Friend</h2>
-    <input type="text" placeholder="Name" />
-    <input type="text" placeholder="Email" />
-    <button class="btn btn-primary">Login</button>
+  <div class="w-screen h-screen flex justify-center items-center">
+    <Login />
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
