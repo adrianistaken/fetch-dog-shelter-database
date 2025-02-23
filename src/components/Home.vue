c93565a441f8af007bcc6257034c8674806c0c36
adrianistaken <adrian.rodriguez.dev@gmail.com>
2025-02-23 10:50:28 -0600

    Added tab navigation for Browse and Favorites

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,40 @@
 <script>
 import Header from './Header.vue';
+import Browse from './Browse.vue';
+import Favorites from './Favorites.vue';
 
 export default {
     components: {
         Header,
+        Browse,
+        Favorites,
     },
+    data() {
+        return {
+            showingSearch: true,
+        }
+    }
 }
 </script>
 
 <template>
-    <div class="bg-white w-screen h-screen">
+    <div class="bg-white min-h-screen w-full">
         <Header />
+        <div class="bg-base w-5/6 min-h-[calc(100vh-8rem)] mx-auto mt-20">
+            <div class="tabs tabs-border">
+                <input type="radio" name="home_tabs" class="tab text-xl bg-base-100 !rounded-b-none" aria-label="Browse"
+                    checked="checked" />
+                <div class="tab-content bg-base-100 p-10">
+                    <Browse />
+                </div>
+
+                <input type="radio" name="home_tabs" class="tab text-xl bg-primary !rounded-b-none"
+                    aria-label="Favorites" />
+                <div class="tab-content bg-primary p-10">
+                    <Favorites />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
