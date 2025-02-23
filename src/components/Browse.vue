c2e98c6ecb4d745111c378ae1b34a01dd1c5dc47
adrianistaken <adrian.rodriguez.dev@gmail.com>
2025-02-23 10:49:47 -0600

    Created simple Browse and Favorites components

---
+++ src/components/Browse.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div>
+        Browse component
+    </div>
+</template>
+
+<style></style>
---
+++ src/components/Favorites.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div>
+        Favorites component
+    </div>
+</template>
+
+<style></style>
