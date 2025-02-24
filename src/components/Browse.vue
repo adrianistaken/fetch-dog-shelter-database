89efa10a3508a20c3b45632c3e65fbb407ed8e3d
adrianistaken <adrian.rodriguez.dev@gmail.com>
2025-02-24 12:15:37 -0600

    Added simple DogCard component

--- src/components/Browse.vue
+++ src/components/Browse.vue
@@ -1,13 +1,52 @@
 <script>
-export default {
+import { ref, onMounted } from "vue";
+import { DogService } from "../../DogService.js";
+import DogCard from "./DogCard.vue";
 
-}
+export default {
+    components: { DogCard },
+    setup() {
+        const dogs = ref([]);
+        const loading = ref(false);
+        const errorMessage = ref("");
+
+        const fetchDogs = async () => {
+            loading.value = true;
+            errorMessage.value = "";
+
+            try {
+                const searchResults = await DogService.searchDogs({
+                    size: 25,
+                    sort: "breed:asc",
+                });
+
+                if (searchResults.resultIds.length > 0) {
+                    dogs.value = await DogService.getDogs(searchResults.resultIds);
+                }
+            } catch (error) {
+                errorMessage.value = "Error fetching dogs.";
+                console.error("Error fetching dogs:", error);
+            } finally {
+                loading.value = false;
+            }
+        };
+
+        onMounted(fetchDogs);
+
+        return { dogs, loading, errorMessage };
+    },
+};
 </script>
 
 <template>
     <div>
-        Browse component
+        <div v-if="loading">Loading dogs...</div>
+        <div v-if="errorMessage" class="">{{ errorMessage }}</div>
+
+        <div class="flex gap-4 flex-wrap" v-if="dogs.length">
+            <DogCard v-for="dog in dogs" :key="dog.id" :dog="dog" />
+        </div>
     </div>
 </template>
 
-<style></style>
+<style scoped></style>
---
+++ src/components/DogCard.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+    props: {
+        dog: Object,
+    },
+};
+</script>
+
+<template>
+    <div class="dog-card">
+        <figure class="image-container">
+            <img :src="dog.img" alt="Dog Image" class="dog-image" />
+        </figure>
+        <div class="card-body p-4 rounded-b-sm">
+            <h2 class="card-title text-2xl">{{ dog.name }}</h2>
+            <p>Breed: {{ dog.breed }}</p>
+            <p>Age: {{ dog.age }}</p>
+            <div class="card-actions justify-start">
+                <button class="btn btn-primary">Favorite</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.dog-card {
+    width: 200px;
+    height: 300px;
+    margin: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    background-color: white;
+    color: black;
+    border-radius: 4px;
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
+}
+
+.image-container {
+    width: 100%;
+    height: 200px;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+}
+
+.dog-image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 4px;
+    border-radius: 4px 4px 0px 0px;
+}
+</style>
